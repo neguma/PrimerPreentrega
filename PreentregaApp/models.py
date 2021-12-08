9c8f1c1aefc692781b57dc5bfc4d94ddd11cf4e7
vdicosmo <vdicosmo1@gmail.com>
2021-12-08 17:05:33 -0300

    Commit2

--- PreentregaApp/models.py
+++ PreentregaApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class guitarra(models.Model):
+    marca = models.CharField(max_length=15)
+    modelo = models.CharField(max_length=15)
+    cant_cuerdas = models.IntegerField()
+
+class mastil(models.Model):
+    madera=models.CharField(max_length=15)
+    numero=models.IntegerField()
+    trastes=models.IntegerField()
+
+class cuerdas(models.Model):
+    marca=models.CharField(max_length=15)
+    grosor=models.IntegerField()
+    tipo=models.CharField(max_length=15)
---
+++ README.md
@@ -0,0 +1,9 @@
+PREENTREGA 1 CURSO PYTHON
+
+ALMUNO NICOLÁS GUMA
+
+README:
+
+1) Iniciar PIPENV
+2) Página principal con enlaces a páginas secundarias
+3) Crear superuser
