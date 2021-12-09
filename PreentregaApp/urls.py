62fc96a33f0eb9a7f5dca969dc660efb4588db3b
neguma <guma.nicolas@gmail.com>
2021-12-09 15:52:54 -0300

    Commit9

--- PreentregaApp/urls.py
+++ PreentregaApp/urls.py
@@ -1,7 +1,11 @@
+from django import template
 from django.urls.conf import path
-from .views import inicio
+from .views import inicio, guitarras, cuerdas, mastil
 
 
 urlpatterns=[
-    path('', inicio, name='inicio')
+    path('', inicio, name='inicio'),
+    path('guitarras/', guitarras, name='guitarras'),
+    path('cuerdas/', cuerdas, name='cuerdas'),
+    path('mastil/', mastil, name='mastil'),
 ]
--- PreentregaApp/views.py
+++ PreentregaApp/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request, 'PreentregaApp/index.html', {})
+
+def guitarras(request):
+    return render(request, 'PreentregaApp/guitarras.html', {})
+
+def cuerdas(request):
+    return render(request, 'PreentregaApp/cuerdas.html', {})
+
+def mastil(request):
+    return render(request, 'PreentregaApp/mastil.html', {})
